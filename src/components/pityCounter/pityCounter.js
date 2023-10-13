c0e69435d83803e1be962af811b400db76ea053e
Gabriela Fernandes Almeida Costa <Gabriela.Costa1@ibm.com>
2023-10-13 15:12:36 -0300

    feat: adding historicWish

--- src/components/pityCounter/pityCounter.js
+++ src/components/pityCounter/pityCounter.js
@@ -3,15 +3,43 @@ import React, { useState } from "react";
 import NewWish from "../wish/newWish";
 
 import "./pityCounter.css";
+import HistoricWish from "../wish/historicWish";
+
+const INITIAL_WISHES = [
+  {
+    id: "1",
+    name: "Guia de Magia",
+    star: "4*",
+    date: new Date(2020, 7, 14),
+    banner: "",
+  },
+  { 
+    id: "2", 
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) },
+  {
+    id: "3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const PityCounter = (props) => {
-  const [wishes, setWishes] = useState([]);
- 
-  const addWishHandler = wish => {
+  const [wishes, setWishes] = useState(INITIAL_WISHES);
+
+  const addWishHandler = (wish) => {
     setWishes((prevWish) => {
-      return[wish, ...wishes];
+      return [wish, ...wishes];
     });
-  }
+  };
 
   return (
     <div className="pity-counter">
@@ -29,36 +57,7 @@ const PityCounter = (props) => {
       </div>
       <div className="history">
         <h3>Histórico de Orações:</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Estrelas</th>
-              <th>Banner</th>
-              <th>Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-            </tr>
-            <tr>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-            </tr>
-            <tr>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-              <td>Exemplo</td>
-            </tr>
-          </tbody>
-        </table>
+        <HistoricWish items={wishes} />
       </div>
     </div>
   );
---
+++ src/components/wish/historicWish.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const HistoricWish = (props) => {
+    return(
+        <div>
+             <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Estrelas</th>
+              <th>Banner</th>
+              <th>Data</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+            </tr>
+            <tr>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+            </tr>
+            <tr>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+              <td>Exemplo</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+    )
+}
+
+export default HistoricWish;
